6a63a74ee9862246dd51e294418305aeb88700cb
Aryan <chaudharyaryan3456@gmail.com>
2025-01-19 00:00:04 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-// import Card from 'react-bootstrap/Card';
-// import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState } from 'react';
 
-const Home= () => {
-  return (
-    <div>
-        
+const Home = () => {
+  // const [age, setAge] = useState("20");
+  const [brand, setBrand] = useState("BMW");
+  const [model, setModel] = useState("GTR3");
+  const [year, setYear] = useState("2005");
+  const [color, setColor] = useState("white");
+  return (<div>
+        <h3>This is my {brand} Car.</h3>
+       <p>Its a {color} {model} from {year} .....</p>
     </div>
   )
 }
